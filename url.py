a5b7441d4172a580e7f011da0673ad2c3013a683
Lucas Schweickert <lucassc@kth.se>
2015-11-04 14:22:34 +0000

    URL.py now accepts multiple word queries

--- url.py
+++ url.py
@@ -1,4 +1,4 @@
-'''This function is supposed to create a urld that returns search results from arxiv
+'''This function is supposed to create a url that returns search results from arxiv
 Input
 =====
 Can only be a string!
@@ -7,8 +7,6 @@ kind can be 'all' for all
             'abs' for abstracts
 
 '''
-
-
-def create(method='search_query', kind='all', query='electron', start='0', number='10'):    
-    url='http://export.arxiv.org/api/query?'+ method +'='+ kind +':'+ query +'&start='+ start +'&max_results='+ number
+def create(method='search_query', kind='all', query='electron photon', start='0', number='10'):    
+    url='http://export.arxiv.org/api/query?'+ method +'='+ kind +':'+ query.replace(' ','+AND+'+kind+':') +'&start='+ start +'&max_results='+ number
     return(url)
