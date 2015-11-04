b72e03254f14aa68c4c1e444656530b7ff043de3
Jasper van Veen <j.vanveen@tudelft.nl>
2015-11-04 14:31:17 +0000

    added two new functions

--- process.py
+++ process.py
@@ -1,10 +1,22 @@
 import feedparser
 
 def extract_totalentries(url):
-	"""This function extracts the number of results in the search query
+	"""This function extracts the number of results in the search query contained in the url
 
 	"""
 	d = feedparser.parse(url)
 	return len(d["entries"])
 
 
+def extract_abstract(url, entry_number):
+	"""This function extract the entry_number-th abstract of the search query contained in the url
+
+	""" 
+	d = feedparser.parse(url)
+	return d["entries"][entry_number]["summary"] 
+
+def extract_title(url, entry_number):
+	"This function is cool"
+
+	d = feedparser.parse(url)
+	return d["entries"][entry_number]["title"]
