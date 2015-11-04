9b266a6bfa4f431cf93d8d086ee576272ada265b
Jasper van Veen <j.vanveen@tudelft.nl>
2015-11-04 14:55:37 +0000

    added function that extracts the date

--- process.py
+++ process.py
@@ -20,3 +20,16 @@ def extract_title(url, entry_number):
 
 	d = feedparser.parse(url)
 	return d["entries"][entry_number]["title"]
+
+def extract_date(url, entry_number, layer=0):
+    """This function extract the date that the paper is submitted to Arxiv
+    
+    Layer specifies the year/month/day ect, default is year
+    0: year
+    1: month
+    2: day
+    """
+    
+    d = feedparser.parse(url)
+    return d['entries'][entry_number]['updated_parsed'][0]
+
